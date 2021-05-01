b97720f05cef06533ba97f7f8f66a2670714154e
Cybertooth99 <parteban.gk@gmail.com>
2021-05-01 10:09:54 +0530

    killer.java changed from void to in return type

--- HelloWorld/src/com/helloworld/Killer.java
+++ HelloWorld/src/com/helloworld/Killer.java
@@ -2,8 +2,9 @@ package com.helloworld;
 
 public class Killer {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		System.out.print("Hello world");
+		return 0;
 	}
 
 }
