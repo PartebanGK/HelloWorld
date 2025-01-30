930fed4f49f2dc442a402fc7b4d6333882d55fab
Parteban <67939977+PartebanGK@users.noreply.github.com>
2025-01-30 11:28:25 +0530

    Hello World

--- HelloWorld/src/com/helloworld/Main.java
+++ HelloWorld/src/com/helloworld/Main.java
@@ -1,25 +1,8 @@
 package com.helloworld;
 
-import java.sql.*;
-
 public class Main {
 
 	public static void main(String[] args) {
-		try 
-		{
-			Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/hello","root","Admin@2021");  
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery("select * from name");
-			while(rs.next())
-			{
-				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3));
-			}
-			connection.close();
-		}
-		catch(Exception e)
-		{
-			System.out.println(e);
-		}
+		System.out.println("Hello World!");
 	}
 }
